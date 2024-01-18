e6c343ab0d7c0d894909cdcdf2a094e39950570c
Théo Larsimont <larsimontheo@gmail.com>
2024-01-18 12:25:19 +0100

    fin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}",
-    "flowbite/**/*.js"],
+    "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       minWidth: {
